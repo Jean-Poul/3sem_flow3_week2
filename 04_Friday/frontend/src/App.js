4d21cfa5bc6246f11547132e41cdf7eee7bdbc80
Jean-Poul <cph-jl360@cphbusiness.dk>
2020-11-06 15:46:02 +0100

    login for frontend added

--- 04_Friday/frontend/src/App.js
+++ 04_Friday/frontend/src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style2.css";
+import AllJokes from "./AllJokes";
+import AllScrape from "./AllScrape";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  useHistory,
   NavLink,
 } from "react-router-dom";
-import "./style2.css";
-import AllJokes from "./AllJokes";
-import AllScrape from "./AllScrape";
 
 // This site has 3 pages, all of which are rendered
 // dynamically in the browser (not server rendered).
@@ -17,43 +18,7 @@ import AllScrape from "./AllScrape";
 // through the site. This preserves the browser history,
 // making sure things like the back button and bookmarks
 // work properly.
-
-export default function BasicExample() {
-  return (
-    <Router>
-      <div>
-        <Header />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-
-        <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/jokes">
-              <Jokes />
-            </Route>
-            <Route path="/scrape">
-              <Scrape />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  );
-}
-
-// You can think of these components as "pages"
-// in your app.
-
-const Header = () => {
+const Header = ({ isLoggedIn, loginMsg }) => {
   return (
     <ul className="header">
       <li>
@@ -66,14 +31,86 @@ const Header = () => {
           Jokes
         </NavLink>
       </li>
+      {isLoggedIn && (
+        <>
+          <li>
+            <NavLink activeClassName="selected" to="/scrape">
+              Scrape
+            </NavLink>
+          </li>
+        </>
+      )}
       <li>
-        <NavLink activeClassName="selected" to="/scrape">
-          Scrape
+        <NavLink activeClassName="selected" to="/login-out">
+          {loginMsg}
         </NavLink>
       </li>
     </ul>
   );
 };
+
+function Login({ isLoggedIn, loginMsg, setLoginStatus }) {
+  const handleBtnClick = () => {
+    setLoginStatus(!isLoggedIn);
+  };
+  return (
+    <div>
+      <h2>{loginMsg}</h2>
+      <button onClick={handleBtnClick}>{loginMsg}</button>
+    </div>
+  );
+}
+
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //let history = useHistory();
+
+  const setLoginStatus = (status) => {
+    setIsLoggedIn(status);
+    //history.push("/");
+  };
+  return (
+    <Router>
+      <div>
+        <Header
+          loginMsg={isLoggedIn ? "Logout" : "Login"}
+          isLoggedIn={isLoggedIn}
+        />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/jokes">
+              <Jokes />
+            </Route>
+            <Route path="/scrape">
+              <Scrape />
+            </Route>
+            <Route path="/login-out">
+              <Login
+                loginMsg={isLoggedIn ? "Logout" : "Login"}
+                isLoggedIn={isLoggedIn}
+                setLoginStatus={setLoginStatus}
+              />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
 
 function Home() {
   return (
